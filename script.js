98a2533c9bc1ee689c49217b5c81ee62f1f4665c
Dhruv Mehta <Dhruv.Mehta@finova.tech>
2024-03-25 13:44:53 +0530

    Minor Changes.

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const name = document.getElementById("name");
 const userLink = document.getElementById("userLink");
 const login = document.getElementById("login");
 const created_at = document.getElementById("created_at");
+const bio = document.getElementById("bio");
+const repoCount = document.getElementById("repoCount");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const userlocation = document.getElementById("location");
+const twitter_username = document.getElementById("twitter_username");
+const company = document.getElementById("company");
 
 search.addEventListener("click", (e) => {
   e.preventDefault();
@@ -25,4 +32,18 @@ async function getUserInfo(user) {
 
   const createddate = data.created_at;
   created_at.innerHTML = createddate.split("T")[0];
+
+  bio.innerHTML = data.bio;
+  repoCount.innerHTML = data.repoCount;
+
+  followers.innerHTML = data.followers;
+
+  following.innerHTML = data.following;
+  userlocation.innerHTML = data.location;
+  if(twitter_username){
+    twitter_username.href = `https://x.com/${data.twitter_username}`;
+    twitter_username.innerHTML = data.twitter_username;
+  }
+  company.innerHTML = data.company;
+
 }
