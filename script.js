adc9d35edbca0e862ddc460c911b034a4edb0dec
Dhruv Mehta <Dhruv.Mehta@finova.tech>
2024-03-26 11:18:26 +0530

    Minor Additions..

--- script.js
+++ script.js
@@ -34,18 +34,38 @@ async function getUserInfo(user) {
   const createddate = data.created_at;
   created_at.innerHTML = createddate.split("T")[0];
 
-  bio.innerHTML = data.bio;
+  if (data.bio == null) {
+    bio.innerHTML = "This Profile has no bio..";
+  } else {
+    bio.innerHTML = data.bio;
+  }
+
   repoCount.innerHTML = data.public_repos;
 
   followers.innerHTML = data.followers;
 
   following.innerHTML = data.following;
-  userlocation.innerHTML = data.location;
-  if(twitter_username){
-    twitter_username.href = `https://x.com/${data.twitter_username}`;
-    twitter_username.innerHTML = data.twitter_username;
-  }
-  company.innerHTML = data.company;
-  profileLink.href = data.html_url;
 
+  if (data.location == null) {
+    userlocation.innerHTML = "Not Available";
+  } else {
+    userlocation.innerHTML = data.location;
+  }
+
+  if(data.twitter_username === null){
+    twitter_username.href = `https://x.com`;
+      twitter_username.innerHTML = "@twitter";
+  }
+  else{
+    twitter_username.href = `https://x.com/${data.twitter_username}`;
+      twitter_username.innerHTML = data.twitter_username;
+  }
+
+  if (data.company === null) {
+    company.innerHTML = "N.A."
+  }
+  else{
+    company.innerHTML = data.company;
+    profileLink.href = data.html_url;
+  }
 }
